4f087c945af15df56eba02534cf7ebb07c18ec65
Rubi <untalrubi@gmail.com>
2024-11-21 18:39:41 +0100

    feat(KATA): :art: Finish Sum Array Singles
    Finished the script to pass the test cases for the sum of array singles kata.

--- src/sum_of_array_singles.py
+++ src/sum_of_array_singles.py
@@ -4,5 +4,12 @@ In this Kata, you will be given an array of numbers in which two numbers occur o
 For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
 '''
 def sum_arrays(arr):
-    #Code Here
-    pass
+
+    sum_result = 0
+    for current_position in range(len(arr)):
+        if arr.count(arr[current_position]) > 1:
+            continue
+        else:
+            sum_result += arr[current_position]
+
+    return sum_result
