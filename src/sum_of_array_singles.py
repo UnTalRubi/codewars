2e9c0d04c929d6b3dc6a7e66c0eb090549cf4662
Rubi <untalrubi@gmail.com>
2024-11-21 18:42:53 +0100

    refactor(KATA): :recycle: Refactor Sum of Array Singles
    Refactored the sum of array singles script using comprehensions.

--- src/sum_of_array_singles.py
+++ src/sum_of_array_singles.py
@@ -5,11 +5,4 @@ For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur 
 '''
 def sum_arrays(arr):
 
-    sum_result = 0
-    for current_position in range(len(arr)):
-        if arr.count(arr[current_position]) > 1:
-            continue
-        else:
-            sum_result += arr[current_position]
-
-    return sum_result
+    return sum (current_position for current_position in arr if arr.count(current_position) == 1)
