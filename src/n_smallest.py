ab1dedecfb4ed5e836e97121644a2a1e5a74714f
Rubi Quintas Alonso <untalrubi@gmail.com>
2024-12-01 17:28:01 +0100

    feat(KATA): :tada: Add First N Smallest kata
    Added new kata with its tests and empty function.

---
+++ src/n_smallest.py
@@ -0,0 +1,12 @@
+'''
+Your task is to write a function that does just what the title suggests (so, fair warning, be aware that you are not getting out of it just throwing a lame basic sorting method there) with a list of integers and the expected number n of smallest elements to return.
+
+Also:
+
+the number of elements to be returned cannot be higher than the list length;
+elements can be duplicated;
+in case of duplicates, just return them according to the original order (see third example for more clarity).
+'''
+def first_n_smallest(arr, n):
+    #My code here
+    pass
